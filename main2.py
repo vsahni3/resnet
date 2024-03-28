b7c3e464f18809c28bbfd78f21aef47819f0603e
Varun Sahni <varunsahni3504@gmail.com>
2024-03-28 13:41:43 -0400

    save model

--- main2.py
+++ main2.py
@@ -48,11 +48,9 @@ def load_images(directory):
         filepath = os.path.join(directory, filename)
         identifier = '_'.join(filepath.split('_')[:3])
         if filepath[-3:] != '.fg':
-            try:
-                inputs.append(filepath)
-            except:
-                print(f'Invalid file path {filepath}')
-                return
+         
+            inputs.append(filepath)
+        
             fg_file = identifier + '.fg'
             label = parse_fg_file(fg_file)
             label = list(label['symmetric_shape_modes']) + list(label['asymmetric_shape_modes']) + list(label['symmetric_texture_modes'])
@@ -85,7 +83,7 @@ def reset_weights(m):
 
 
 
-def train_model(directory):
+def train_model(directory, pretrained=False, model_path=''):
 
     inputs, labels = load_images(directory)
 
@@ -100,7 +98,6 @@ def train_model(directory):
 
     dataset = CustomDataset(inputs, labels, scaling_constant, transform=transform)
 
-    # Let's assume `dataset` is an instance of your CustomDataset
     dataset_size = len(dataset)
     train_size = int(dataset_size * 0.8)  # 80% of the dataset for training
     test_size = dataset_size - train_size  # The remaining 20% for testing
@@ -113,8 +110,10 @@ def train_model(directory):
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 
-    epochs = 1000
+    epochs = 1500
     batch_size = 50
+
+    # change model architecture
 
     resnet = InceptionResnetV1(pretrained='casia-webface')
 
@@ -123,10 +122,22 @@ def train_model(directory):
     resnet.last_bn = torch.nn.BatchNorm1d(130)
     resnet.apply(reset_weights)
 
+    if pretrained:
+        optimizer, cur_epoch = load_partially_trained_model(model_path, resnet)
+    else:
+        optimizer = torch.optim.SGD(resnet.parameters(), lr=0.01, momentum=0.9)
+        cur_epoch = 0
 
-    initial_weights = resnet.last_linear.weight.data.clone()
-    optimizer = torch.optim.SGD(resnet.parameters(), lr=0.01, momentum=0.9)
-    for epoch in range(epochs):
+    running_loss = 0
+    count = 0
+
+    big_loss = 0
+    big_count = 0
+
+
+    
+
+    for epoch in range(cur_epoch, epochs):
         
         
         for inputs, labels in train_loader:
@@ -142,26 +153,61 @@ def train_model(directory):
             loss.backward()
 
             optimizer.step()
-            print(loss.item())
-        # if epoch % 2 == 0:
-        #     # After some training, check the weights
-        #     trained_weights = resnet.last_linear.weight.data.clone()
-        #     print(loss_function(initial_weights, trained_weights))
+            
+            running_loss += loss.item()
+            big_loss += loss.item()
+            count += 1
+            big_count += 1
+        if epoch % 5 == 0:
+            print(f'Average loss: {running_loss / count}')
+            count = 0
+            running_loss = 0
+            # After some training, check the weights
+            # trained_weights = resnet.last_linear.weight.data.clone()
+            # print(loss_function(initial_weights, trained_weights))
       
-        #     # Compare the initial and trained weights for any layer
-        #     if not torch.equal(initial_weights, trained_weights):
-        #         print("Weights have been updated.")
-        #     else:
-        #         print("Weights have not changed.")
-        #     # embeddings_end = resnet(aligned_og)
+            # # Compare the initial and trained weights for any layer
+            # if not torch.equal(initial_weights, trained_weights):
+            #     print("Weights have been updated.")
+            # else:
+            #     print("Weights have not changed.")
+            # embeddings_end = resnet(aligned_og)
 
 
-        #     # print(loss_function(embeddings_initial, embeddings_end))
-        #     # print(embeddings_initial, end='\n\n\n')
-        #     # print(embeddings_end)
+            # print(loss_function(embeddings_initial, embeddings_end))
+            # print(embeddings_initial, end='\n\n\n')
+            # print(embeddings_end)
+    
+        if (epoch + 1) % 100 == 0:
+  
+            checkpoint_filename = f'checkpoint_epoch_{epoch+1}.pth'
+            
+            torch.save({
+                'epoch': epoch+1,
+                'model_state_dict': resnet.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': big_loss / big_count,
+            }, checkpoint_filename)
+            print(f"Saved checkpoint to {checkpoint_filename}")
+            big_count = 0
+            big_loss = 0
+
+def load_partially_trained_model(model_path, model):
+    checkpoint = torch.load(model_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    # Now, initialize the optimizer with the model's updated parameters
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+    # Load the optimizer state dict
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    loss = checkpoint['loss']  
+    epoch = checkpoint['epoch']
+    print(f'Current loss: {loss}') 
+    return optimizer, epoch
 
 
-train_model('Archive')
+
+train_model('Archive', True, 'checkpoint_epoch_500.pth')
 # # Load pre-trained Inception ResNet model
 # resnet = InceptionResnetV1(pretrained='casia-webface')
 
